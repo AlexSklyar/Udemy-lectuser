774132a60c3510d43450187a4bfef0bf512cca6b
Alex <80081422+AlexSklyar@users.noreply.github.com>
2021-05-07 13:26:57 +0300

    calc
    now my calc is done and i will start new application

---
+++ Calc is done/assets/scripts/app.js
@@ -0,0 +1,71 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = []
+function getUserNumberInput() {
+return +userInput.value
+} 
+function crateAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const currentDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, currentDescription);
+}
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+const logEntry = {
+        operation: operationIdentifier,
+        prevresult: prevResult,
+        number: operationNumber,
+        result: newResult        
+        };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+function calculateResult(calculationType) {
+     const enterNumber = getUserNumberInput();
+    if (calculationType !== 'ADD' &&
+        calculationType !== 'SUBTRACT' &&
+        calculationType !== 'MULTYPLY' &&
+        calculationType !== 'DIVIDE' ||
+        !enterNumber
+    ) {
+        return;
+    }
+    // if (calculationType === 'ADD' ||
+    //     calculationType === 'SUBTRACT' ||
+    //     calculationType === 'MULTYPLY' ||
+    //     calculationType === 'DIVIDE'
+    // ) {
+   
+    const initialResult = currentResult;
+    let mathOperator;
+    if (calculationType === 'ADD') {
+        currentResult += enterNumber;
+        mathOperator = '+';
+    } else if (calculationType === 'SUBTRACT') {
+        currentResult -= enterNumber;
+        mathOperator = '-';
+    } else if (calculationType === 'MULTYPLY') {
+        currentResult *= enterNumber;
+        mathOperator = '*';
+    } else {
+        currentResult /= enterNumber;
+        mathOperator = '/';
+    }
+    crateAndWriteOutput(mathOperator, initialResult, enterNumber);
+    writeToLog(calculationType, initialResult, enterNumber, currentResult);
+    // }
+}
+function add() {
+calculateResult('ADD')
+}
+function subtract() {
+calculateResult('SUBTRACT')
+}
+function multiply() {
+calculateResult('MULTYPLY')
+}
+function divide() {
+calculateResult('DIVIDE')
+}
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
